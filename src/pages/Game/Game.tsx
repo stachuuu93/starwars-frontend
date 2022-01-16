19058f989872b898aa842b7b2845bdd4092fdde4
Stanislaw Nowak <stanislaw.nowak@partners.fortum.com>
2022-01-16 22:22:01 +0100

    Add play again button

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
-import { CircularProgress, Grid } from "@mui/material";
+import { Button, CircularProgress, Grid } from "@mui/material";
 
 import PlayCard from "../../components/PlayCard/PlayCard";
+import { purple } from "@mui/material/colors";
 
 const RANDOM_SHIPS = gql`
   query getRandomShips($limit: Int) {
@@ -17,7 +18,7 @@ const RANDOM_SHIPS = gql`
 `;
 
 const Game = () => {
-  const { error, loading, data } = useQuery(RANDOM_SHIPS, {
+  const { error, loading, data, refetch } = useQuery(RANDOM_SHIPS, {
     fetchPolicy: "no-cache",
     variables: { limit: 2 },
   });
@@ -35,14 +36,31 @@ const Game = () => {
   const { imageUrl: leftStarshipImage, ...leftStarshipAttrs } = leftStarship;
   const { imageUrl: rightStarshipImage, ...rightStarshipAttrs } = rightStarship;
 
+  const playAgain = () => {
+    refetch();
+  };
+
   return (
-    <Grid container justifyContent="space-around">
+    <Grid container justifyContent="space-around" alignItems="center">
       <Grid item xs={5} md={3} lg={2}>
         <PlayCard
           type="starship"
           attributes={leftStarshipAttrs}
           imageUrl={leftStarshipImage}
         />
+      </Grid>
+      <Grid
+        item
+        container
+        xs={1}
+        md={2}
+        bgcolor={purple[900]}
+        justifyContent="center"
+        p={2}
+      >
+        <Button variant="contained" onClick={playAgain}>
+          Play again
+        </Button>
       </Grid>
       <Grid item xs={5} md={3} lg={2}>
         <PlayCard
